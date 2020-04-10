9fc7ccda4238128a5be39a71edc6b4937475eed4
danysaavedra <danielesaavedra2015@gmail.com>
2020-04-10 07:55:42 -0300

    emiliayMetegol

---
+++ Emilia/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700|Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/home.css">
+    <span style="font-size: 3em; color: Tomato;">
+  <i class="fas fa-camera"></i>
+</span>
+
+<span style="font-size: 48px; color: Dodgerblue;">
+  <i class="fas fa-camera"></i>
+</span>
+
+<span style="font-size: 3rem;">
+  <span style="color: Mediumslateblue;">
+  <i class="fas fa-camera"></i>
+  </span>
+</span>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <nav>
+                    <ul>
+                    <li>
+                       <a href="home.php">HOME</a>
+                    </li>
+                    <li>
+                        <a href="about.php"> ABOUT</a>
+                    </li>
+                    <li>
+                        <a href="music.php">MUSIC</a>
+                    </li>
+                    <li>
+                        <a href="contact.php"> CONTACT</a>
+                    </li>
+                 </ul>
+                    <div class="redes">
+                        <img src="img/twitter.png" alt="twitter">
+                        <em style="font-size: 10px; color: Tomato;">
+  <i class="fas fa-camera"></i>
+</em>
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </div>
+                   
+                    <h2>EMILIA COLE//</h2>
+            </nav>
+        
+                <article>
+                    
+                    
+                    <div class="emilia1">
+                        <img src="img/emilia-home.jpg" alt="emilia1">
+                    </div>
+                    <h1>TWIST IN MY REALITY</h1>
+                </article>
+              
+            
+        </header>
+    </div>
+</body>
+</html>
---
+++ Metegol/principal.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Metegol</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+   <div  class="container">
+   <h1>Fue director de efectos visuales de Metegol,<br> creó una empresa y triunfa en el mundo</h1>
+       <section>
+       
+           <article>
+            <p class="bajada">Paolo Cavalieri fundó Bleed VFX, dedicada a la creación de efectos especiales y que ya cuenta con oficinas en Buenos Aires, Barcelona y San Pablo</p>
+                <img src="img/metegol.jpg" alt="metegol">
+<br>
+<p>La historia de este joven de 32 años oriundo de Cañada de Gómez, Santa Fe y la empresa que lo posicionó como una figura de peso en el mercado de la posproducción digital a nivel mundial empezó apenas una década atrás, cuando daba sus primeros pasos en la industria y diseñaba una cátedra propia en la escuela de producción Image Campus.
+<br>
+"Trabajaba como empleado en una casa de posproducción y al mismo tiempo daba clases", cuenta Cavalieri a <strong>LA NACIÓN.</strong> "Uno de mis alumnos conocía a mi actual socio y me dijo 'juntémonos, podemos hacer algo propio entre los tres'. Tuvimos una reunión inicial y empezamos a darle forma, hasta que un día los tres abandonamos nuestros trabajos y nos lanzamos a hacer este emprendimiento."</p>
+           </article>
+           <article>
+           <h2>Una propuesta especializada</h2>
+           <p>Para diferenciarse de sus competidores, Cavalieri decidió ofrecer a sus prospectivos clientes -marcas, agencias publicitarias y/o productoras- un portfolio de servicios enfocado en su área de expertise: las simulaciones dinámicas. Con este nombre se denomina en el medio a las tecnologías que permiten recrear visualmente elementos como fuego, humo, telas, pelos y partículas, entre otros.
+<br>
+Además, la empresa brinda servicios de posproducción global, como animación de personajes y recorte de croma -esos telones de color verde o azul que se usan para agregar fondos distintos en una escena-. Las aplicaciones se ven en publicidad, cine, infomerciales, branding de canales y videos musicales.</p>
+           </article>
+           <article>
+           <h2>Un rápido crecimiento</h2>
+           <p>"Fui afortunado", confiesa Cavalieri. "Tenía un nombre en el mercado y las compañías con las que había trabajado antes confiaban en mí", agregó. La reputación positiva de Bleed VFX se fue consolidando y pronto compañías multinacionales como Coca Cola, Procter & Gamble (P&G), Unilever, Mondelez y Disney comenzaron a llamar a su puerta.</p>
+           </article>
+       </section>
+   </div>
+    
+</body>
+</html>
