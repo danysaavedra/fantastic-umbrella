526c84d3c12bacba7f3a28616286909e796a054a
danysaavedra <danielesaavedra2015@gmail.com>
2021-06-08 17:21:55 -0300

    commit

--- juegoQuizzLaravel/resources/views/layouts/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($errors->any())
-<div class="alert alert-danger text-center" role="alert">
-    <strong>
-        @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-        @endforeach
-    </strong>
-</div>
-@endif
